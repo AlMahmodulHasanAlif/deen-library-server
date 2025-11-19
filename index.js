83a740bd78853f6a032e397d8618d3cb57bbd1bf
Al Mahmodul Hasan Alif <almahmodulhasanalif@gmail.com>
2025-11-19 08:36:00 +0600

    MongoDB connection established

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log("USER:", process.env.MONGO_USER);
+console.log("PASS:", process.env.MONGO_PASS);
+
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
+
+//mongoURI
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ill4a2j.mongodb.net/?appName=Cluster0`;
+
+//mongoDB
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
